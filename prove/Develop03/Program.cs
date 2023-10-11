d61c36937f5b1003a482c845d8c72e295122146b
Kaleb Trogdon <67351277+KalebTrogdon@users.noreply.github.com>
2023-10-10 19:57:37 -0600

    new Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,33 @@
-using System;
-
+// Entry point of the program
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create instances of the Fraction class
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(1, 3);
+
+        // Create an instance of the FractionCalculator class
+        FractionCalculator calculator = new FractionCalculator();
+
+        // Perform arithmetic operations and display results
+        Fraction sum = calculator.Add(fraction1, fraction2);
+        Console.WriteLine("Sum: " + sum.GetFractionString());
+
+        Fraction difference = calculator.Subtract(fraction1, fraction2);
+        Console.WriteLine("Difference: " + difference.GetFractionString());
+
+        Fraction product = calculator.Multiply(fraction1, fraction2);
+        Console.WriteLine("Product: " + product.GetFractionString());
+
+        try
+        {
+            Fraction quotient = calculator.Divide(fraction1, fraction2);
+            Console.WriteLine("Quotient: " + quotient.GetFractionString());
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
     }
 }
